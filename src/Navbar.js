7e93919ec643bcf16b18186ad6f7027928c4c9e4
samuel <samuelincoln1@gmail.com>
2024-04-29 14:42:47 -0300

    basic components navbar homepage

---
+++ src/Navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+    return ( 
+        <nav className="navbar">
+            <h1>Blog teste</h1>
+            <div className="links">
+                <a href="/">Home</a>
+                <a href="/create">Criar post</a>
+            </div>
+        </nav>
+     );
+}
+ 
+export default Navbar;
