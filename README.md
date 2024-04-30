b808d4128643e0c3b5c0c9fe65796b95e86cd0cb
samuel <samuelincoln1@gmail.com>
2024-04-30 14:59:39 -0300

    mostrando lista de dados na tela

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 Blog simples para aprender react
+    
--- src/Home.js
+++ src/Home.js
@@ -2,19 +2,36 @@
 import { useState } from "react";
 
 const Home = () => {
-
-    // let name = 'samuel';
-    const [name, setName] = useState('samuel');
-
-    const handleClick = () => {
-        setName('samuel dois');
-    }
+    const [blogs, setBlogs] = useState([
+        {
+            title: 'Novo website',
+            body: 'lorem ipsum...',
+            author: 'samuel',
+            id: 1
+        },
+        {
+            title: 'Festa de boas vindas!',
+            body: 'lorem ipsum...',
+            author: 'mario',
+            id: 2
+        },
+        {
+            title: 'Novidades de tecnologia',
+            body: 'lorem ipsum...',
+            author: 'pedro',
+            id: 3
+        },
+    ]);
+   
 
     return ( 
         <div className="home">
-            <h2>HomePage</h2>´
-            <p>{name}</p>
-            <button onClick={handleClick}>Clicar</button>
+           {blogs.map((blog) => (
+                <dib className="blog-preview" key={blog.id}>
+                    <h2>{blog.title}</h2>
+                    <p>Escrito por {blog.author}</p>
+                </dib>
+           ))}
            
         </div>
      );
